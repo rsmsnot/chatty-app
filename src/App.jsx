cc15710e005d620fc9b8fa14861cf96a16a885b8
rsmsnot <rsmsnot@outlook.com>
2019-07-03 00:11:08 +0000

    added loop through messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,44 @@
 import React, { Component } from 'react';
 import ChatBar from './ChatBar.jsx'
-import MessageList from './MessageList.jsx' 
+import MessageList from './MessageList.jsx'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: { name: 'Bob' }, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          id: 0,
+          username: 'Bob',
+          content: 'Has anyone seen my marbles?',
+        },
+        {
+          id: 1,
+          username: 'Anonymous',
+          content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.'
+        }
+      ]
+    }
+  }
+    componentDidMount() {
+      setTimeout(() => {
+        // Add a new message to the list of messages in the data store
+        const newMessage = {id: 3, username: 'Michelle', content: 'Hello there!'};
+        const messages = this.state.messages.concat(newMessage)
+        // Update the state of the app component.
+        // Calling setState will trigger a call to render() in App and all child components.
+        this.setState({messages: messages})
+      }, 3000);
+    }
   render() {
     return (
       <div>
         <nav className="navbar">
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
-        <ChatBar />
-        <MessageList />
+        <ChatBar currentUser={this.state.currentUser} />
+        <MessageList messages={this.state.messages}/>
       </div>
     );
   }
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -3,11 +3,16 @@ import Message from './Message.jsx'
 
 class MessageList extends React.Component {
     render() {
+        const allMessages = this.props.messages.map(message => {
+            return <Message
+                key={message.id}
+                username={message.username}
+                content={message.content} />
+        });
         return (
             <main className="messages">
-                <Message />
+                {allMessages}
                 <div className="message system">
-                    Anonymous1 changed their name to nomnom.
                 </div>
             </main>
         )
