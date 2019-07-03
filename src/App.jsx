528f3f32f30ae3307ffb4a52002c95090c5e489c
rsmsnot <rsmsnot@outlook.com>
2019-07-03 19:09:16 +0000

    installed websocket / connected client to server

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentUser: { name: 'Bob' }, // optional. if currentUser is not defined, it means the user is Anonymous
+      currentUser: { name: 'Anonymous' }, // optional. if currentUser is not defined, it means the user is Anonymous
       messages: [
         {
           id: 0,
@@ -20,25 +20,38 @@ class App extends Component {
         }
       ]
     }
+    this.addNewMessage= this.addNewMessage.bind(this);
   }
-    componentDidMount() {
-      setTimeout(() => {
-        // Add a new message to the list of messages in the data store
-        const newMessage = {id: 3, username: 'Michelle', content: 'Hello there!'};
-        const messages = this.state.messages.concat(newMessage)
-        // Update the state of the app component.
-        // Calling setState will trigger a call to render() in App and all child components.
-        this.setState({messages: messages})
-      }, 3000);
-    }
+
+  addNewMessage(message) {
+    const messages = this.state.messages.concat(message);
+    this.setState({ messages: messages });
+
+  }
+
+
+  componentDidMount() {
+    this.WebSocket = new WebSocket('ws://localhost:3001');
+    this.WebSocket.onopen = function (event) {
+      console.log('Connected to server'); 
+    };
+    // setTimeout(() => {
+    //   // Add a new message to the list of messages in the data store
+    //   const newMessage = { id: 3, username: 'Michelle', content: 'Hello there!' };
+    //   const messages = this.state.messages.concat(newMessage)
+    //   // Update the state of the app component.
+    //   // Calling setState will trigger a call to render() in App and all child components.
+    //   this.setState({ messages: messages })
+    // }, 3000);
+}
   render() {
     return (
       <div>
         <nav className="navbar">
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
-        <ChatBar currentUser={this.state.currentUser} />
-        <MessageList messages={this.state.messages}/>
+        <ChatBar currentUser={this.state.currentUser} addNewMessage={this.addNewMessage}/>
+        <MessageList messages={this.state.messages} />
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -2,13 +2,42 @@ import React, { Component } from 'react';
 
 
 class ChatBar extends React.Component {
+    constructor(props){
+        super(props)
+        this.inputSubmit = this.inputSubmit.bind(this);
+    }
+
+    randomIdGenerator() {
+        return Math.floor(Math.random() * 1000 + 1)
+    }
+
+    inputSubmit(event) {
+        if (event.key === 'Enter') {
+            const messageContent = {
+                id: this.randomIdGenerator(),
+                content: event.target.value,
+                username: this.props.currentUser.name
+            }
+            console.log(event.key, 'pressed');
+            console.log(messageContent);
+            this.props.addNewMessage(messageContent);
+            event.target.value = ' ';
+            // this.props.addNewMessage(this.state.username, this.state.content);
+            // this.setState({
+            //     username: this.state.user,
+            //     content: this.state.content 
+            }
+        }
+    
+
     render() {
-        return(
-        <footer className="chatbar">
-            <input className="chatbar-username" placeholder="Your Name (Optional)" />
-            <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
-        </footer>
-        )}
+        return (
+            <footer className="chatbar">
+                <input className="chatbar-username" placeholder="Your Name (Optional)" />
+                <input className="chatbar-message" placeholder="Type a message and hit ENTER" onKeyPress={this.inputSubmit}/>
+            </footer>
+        )
+    }
 }
 
 export default ChatBar;
