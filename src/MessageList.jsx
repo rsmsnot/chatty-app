0b5f12b273a6a46ea314573ee1e777b1e36a2db7
rsmsnot <rsmsnot@outlook.com>
2019-07-05 01:29:25 +0000

    added function to autoscroll down to newest message

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -4,6 +4,12 @@ import Notification from './Notifications.jsx';
 import Image from './Images.jsx';
 
 class MessageList extends React.Component {
+    scrollToBottom() {
+        this.messagesEnd.scrollIntoView({ behavior: 'smooth' });
+    }
+    componentDidUpdate() {
+        this.scrollToBottom();
+    }
     render() {
         const allMessages = this.props.messages.map(message => {
             if (message.type === 'message') {
@@ -27,7 +33,7 @@ class MessageList extends React.Component {
         return (
             <main className="messages">
                 {allMessages}
-                <div className="message system">
+                <div className="message system" ref={(el) => { this.messagesEnd = el; }}>
                 </div>
             </main>
         )
